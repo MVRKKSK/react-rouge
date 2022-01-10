4bd2df137ce25004c6a9bc59e4e1e6024c6abd44
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-01-10 14:36:46 +0530

    made a movable rect

--- react-rouge/src/Reactrogue.jsx
+++ react-rouge/src/Reactrogue.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React , {useRef , useEffect} from 'react'
 
 const Reactrogue = ({width , height , tilesize}) => {
+    const canvasRef = useRef();
+    useEffect(() => {
+        console.log("hello iam using a game")
+        const ctx = canvasRef.current.getContext("2d");
+        ctx.clearRect(0 , 0 , width*tilesize , height*tilesize);
+        ctx.fillRect(12 , 20 , 16 , 16)
+
+        
+    });
     return (
-        <canvas width={width * tilesize} height = {height*tilesize} style={{border: "1px solid black"}}>
+        <canvas ref={canvasRef} width={width * tilesize} height = {height*tilesize} style={{border: "1px solid black"}}>
           
         </canvas>
     )
